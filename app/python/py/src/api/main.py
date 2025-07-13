97eba120e9b4be2a2f1d2ca4fa947eea92aa94e1
ky381011 <ky381011git@gmail.com>
2025-07-13 12:44:23 +0900

    Enable to choose executing class

--- app/python/py/src/api/main.py
+++ app/python/py/src/api/main.py
@@ -1,12 +1,27 @@
 import sys
+import json
 
-from dev import switchbot
-from dev import OpenWeatherAPI
+from dev.Switchbot import GetSwitchbotStatus
+from dev.OpenWeatherAPI import WeatherFetcher
 
 args = sys.argv
 
 classes = {
-    "hub2": switchbot,
-    "plug": OpenWeatherAPI,
+    "switchbot": GetSwitchbotStatus,
+    "weather": WeatherFetcher,
 }
+
+target = sys.argv[1]
+cls = classes.get(target)
+
+
+
+if cls is None:
+    print(f"未知のターゲット: {target}")
+    sys.exit(1)
+
+instance = cls(args[2])
+status = instance.getStatus()
+
+print(json.dumps(status, indent=2))
 
