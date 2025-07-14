ad128f69218619cad073d6266ab6203d7f452361
ky381011 <ky381011git@gmail.com>
2025-07-14 21:15:16 +0900

    Change to use argparse

--- app/python/py/src/api/main.py
+++ app/python/py/src/api/main.py
@@ -1,27 +1,45 @@
 import sys
 import json
+import argparse
 
 from dev.Switchbot import GetSwitchbotStatus
 from dev.OpenWeatherAPI import WeatherFetcher
-
-args = sys.argv
 
 classes = {
     "switchbot": GetSwitchbotStatus,
     "weather": WeatherFetcher,
 }
 
-target = sys.argv[1]
+parser = argparse.ArgumentParser()
+
+parser.add_argument('target_class', help='Which api to call')
+parser.add_argument('-d', '--deviceId', help='Switchbot device id')
+parser.add_argument('-c', '--city', help='Where to get weather information')
+
+args = parser.parse_args()
+
+target = args.target_class.lower()
+
 cls = classes.get(target)
 
-
-
-if cls is None:
-    print(f"未知のターゲット: {target}")
+if target not in classes:
+    print(f"Unsupported class '{target}'")
     sys.exit(1)
 
-instance = cls(args[2])
-status = instance.getStatus()
+# Instantiate
+if target == "switchbot":
+    if not args.deviceId:
+        print("Error: --deviceId is required for switchbot.")
+        sys.exit(1)
+    instance = classes[target](device_id=args.deviceId)
 
-print(json.dumps(status, indent=2))
+elif target == "weather":
+    if not args.city:
+        print("Error: --city is required for weather.")
+        sys.exit(1)
+    instance = classes[target](city=args.city)
+
+# Call and display result
+result = instance.getStatus()
+print(json.dumps(result, indent=2))
 
