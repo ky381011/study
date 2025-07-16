ea0f9f71410e6e602f104142ad6c6f1e805b0dbb
ky381011 <ky381011git@gmail.com>
2025-07-16 21:45:22 +0900

    Second endpoint

--- app/python/py/src/fastapi/main.py
+++ app/python/py/src/fastapi/main.py
@@ -8,5 +8,9 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/second")
+async def root():
+    return {"message": "Second"}
+
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
