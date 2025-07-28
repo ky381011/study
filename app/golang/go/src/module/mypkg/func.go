cf3c222b9fe9f090135242a2adf2835b2aee880c
ky381011 <ky381011git@gmail.com>
2025-07-28 23:58:36 +0900

    Move dif to func2

--- app/golang/go/src/module/mypkg/func.go
+++ app/golang/go/src/module/mypkg/func.go
@@ -3,11 +3,3 @@ package mypkg
 func Add(x int, y int) int {
 	return x + y
 }
-
-func Dif(x int, y int) int {
-	if x >= y {
-		return x - y
-	} else {
-		return y - x
-	}
-}
---
+++ app/golang/go/src/module/mypkg/func2.go
@@ -0,0 +1,9 @@
+package mypkg
+
+func Dif(x int, y int) int {
+	if x >= y {
+		return x - y
+	} else {
+		return y - x
+	}
+}
