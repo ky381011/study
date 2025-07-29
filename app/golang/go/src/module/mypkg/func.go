1b2a8a04b437551143b80d411b4ce59fd941c81b
ky381011 <ky381011git@gmail.com>
2025-07-29 22:36:00 +0900

    Merged to math

--- app/golang/go/src/module/mypkg/func.go
+++
@@ -1,5 +0,0 @@
-package mypkg
-
-func Add(x int, y int) int {
-	return x + y
-}
--- app/golang/go/src/module/mypkg/math.go
+++ app/golang/go/src/module/mypkg/math.go
@@ -1,5 +1,9 @@
 package mypkg
 
+func Add(x int, y int) int {
+	return x + y
+}
+
 func Dif(x int, y int) int {
 	if x >= y {
 		return x - y
